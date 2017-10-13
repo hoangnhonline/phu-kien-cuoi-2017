5a67d2320cd0e5da6ffc1f6417d6f2c33b82cf89
HoangNH <hoangnhonline@gmail.com>
2017-10-13 16:39:04 +0700

    .

--- storage/framework/views/342230f1e32510b267c4b5489894cdbb6539510a.php
+++
@@ -1,4 +0,0 @@
-<?php echo $__env->make('frontend.partials.meta', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>  
-  
-<?php echo $__env->make('frontend.home.content', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-<?php echo $__env->make('frontend.layout', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
